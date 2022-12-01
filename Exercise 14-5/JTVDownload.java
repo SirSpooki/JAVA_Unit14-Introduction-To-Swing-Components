3378d71dd508292d6d2de41ddfca1c7677b22beb
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-12-01 12:15:47 -0600

    Update JTVDownload.java

--- Exercise 14-5/JTVDownload.java
+++ Exercise 14-5/JTVDownload.java
@@ -1,31 +1,57 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class JTVDownload extends JFrame implements ItemListener {
-    JComboBox<String> programBox = new JComboBox<String>();
-    JLabel programList = new JLabel("***                  Program List               ***");
-    JLabel stars = new JLabel("*****************************");
-    JTextField descripField = new JTextField(20);
-    String[] progs = {"Breaking Bad",
-                      "Orange is the New Black", "Twilight Zone",
-                      "Parks and Recreation", "Everybody Loves Raymond"
-                     };
-    String[] descrips = {"", "Teacher becomes meth dealer",
-                         "Life in women's prison",
-                         "Classic science fiction episodes",
-                         "Comedy in local government office",
-                         "Family comedy with meddling in-laws"
-                        };
-    public JTVDownload() {
-        // Write your code here
+import javax.swing.*;
+public class JTVDownload extends JFrame implements ActionListener {
+  JComboBox<String> programBox = new JComboBox<String>();
+  JLabel programList = new JLabel("*** Program List ***");
+  JLabel stars = new JLabel("*****************************");
+  JTextField descripField = new JTextField(20);
+  // add your desired TV programs to the string
+  String[] progs
+      = { "The Walking Dead", "Gotham", "9-1-1", "This is Us", "ANTM" };
+  // add your desired TV programs descrips to the string
+  String[] descrips = { "", "Humans living in a zombie apocalypse",
+    "Drama series based off Batman", "Series about lives of first responders",
+    "Emotional drama about a family", "Modeling competition" };
+  public JTVDownload()
+  {
+    super("JTVDownload");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new FlowLayout());
+    programBox.addActionListener(this);
+    add(stars);
+    add(programList);
+    programBox.addItem("none");
+    programBox.setEditable(true);
+    for (int x = 0; x < progs.length; ++x)
+      programBox.addItem(progs[x]);
+    add(programBox);
+    add(descripField);
+  }
+  public static void main(String[] arguments)
+  {
+    JTVDownload frame = new JTVDownload();
+    frame.setSize(300, 150);
+    frame.setVisible(true);
+  }
+  @Override public void actionPerformed(ActionEvent list)
+  {
+    Object source = list.getSource();
+    if (source == programBox) {
+      int num = programBox.getSelectedIndex();
+      if (num >= 0) {
+        descripField.setText(descrips[num]);
+      } else if (true) {
+        Object newVal = programBox.getSelectedItem();
+        String str = (String)newVal;
+        for (int i = 0; i < progs.length; i++) {
+          if (progs[i].equals(str)) {
+            descripField.setText(descrips[i]);
+            break;
+          }
+        }
+        descripField.setText("Movie Not Found");
+      }
     }
-    public static void main(String[] arguments) {
-        JTVDownload frame = new JTVDownload();
-        frame.setSize(300, 150);
-        frame.setVisible(true);
-    }
-    @Override
-    public void itemStateChanged(ItemEvent list) {
-        // Write your code here
-    }
+  }
 }
